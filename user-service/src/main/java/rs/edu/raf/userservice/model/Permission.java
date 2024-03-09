3e20a84e63f7e82cbf5f8ceba3c301ef7c11491a
Damir Ceh <dceh9121rn@raf.rs>
2024-03-09 17:27:32 +0100

    Kreiran model sa entitetima User, Permission, Role

---
+++ user-service/src/main/java/rs/edu/raf/userservice/model/Permission.java
@@ -0,0 +1,38 @@
+package rs.edu.raf.userservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import rs.edu.raf.userservice.model.enums.PermissionName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name="permissions")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long permissionId;
+
+    @Enumerated(EnumType.STRING)
+    private PermissionName permissionName;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_permissions",
+            joinColumns = @JoinColumn(name = "permissionId"),
+            inverseJoinColumns = @JoinColumn(name = "userId")
+    )
+    private List<User> users = new ArrayList<>();
+
+}
---
+++ user-service/src/main/java/rs/edu/raf/userservice/model/enums/PermissionName.java
@@ -0,0 +1,9 @@
+package rs.edu.raf.userservice.model.enums;
+
+public enum PermissionName {
+    ADMIN_USER,
+    READ_USERS,
+    CREATE_USERS,
+    UPDATE_USERS,
+    DELETE_USERS
+}
