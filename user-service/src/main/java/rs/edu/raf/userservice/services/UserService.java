130e360ecd9a419fdfff8432461a1d88bdb7381c
Mladen Matic <you@example.com>
2024-03-11 00:41:24 +0100

    Kreiranje UserService i UserRepository.

--- user-service/src/main/java/rs/edu/raf/userservice/services/UserService.java
+++ user-service/src/main/java/rs/edu/raf/userservice/services/UserService.java
@@ -41,6 +41,3 @@ public class UserService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
-
-
-
