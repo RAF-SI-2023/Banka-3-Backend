8fa3ac216993be960859502346958a838e3f0a53
Jelena Drljan <jdrljan4620rn@raf.rs>
2024-03-10 17:53:00 +0100

    BCK-7 integracija swagger

--- user-service/src/main/java/rs/edu/raf/userservice/configuration/SwaggerConfig.java
+++ user-service/src/main/java/rs/edu/raf/userservice/configuration/SwaggerConfig.java
@@ -1,4 +1,32 @@
 package rs.edu.raf.userservice.configuration;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+@Configuration
 public class SwaggerConfig {
+    // Ovo treba da se promeni u naziv mikroservisa.
+    private final String APP_TITLE = "user-service";
+    // Ovo treba da se promeni u neki opis mikroservisa.
+    private final String APP_DESCRIPTION = "API za korisnicki servis";
+
+    // Ovo ispod ostavite kako jeste.
+    private final String APP_API_VERSION = "1.0";
+    private final String APP_LICENSE = "Licenca";
+    private final String APP_LICENSE_URL = "Licenca";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title(APP_TITLE)
+                        .description(APP_DESCRIPTION)
+                        .version(APP_API_VERSION)
+                        .license(new License().name(APP_LICENSE).url(APP_LICENSE_URL)));
+    }
+
 }
--- user-service/src/main/java/rs/edu/raf/userservice/controllers/UserController.java
+++ user-service/src/main/java/rs/edu/raf/userservice/controllers/UserController.java
@@ -1,4 +1,24 @@
 package rs.edu.raf.userservice.controllers;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    public UserController() {
+    }
+
+    @GetMapping
+    public void getAllUsers() {
+    }
+
 }
