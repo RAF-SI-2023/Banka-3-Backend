e0e0ec1b8571a0c19ac44daf440a368aa6ea9779
DusanEric <92939413+DusanEric@users.noreply.github.com>
2024-03-11 20:20:03 +0100

    BCK-2 kreiranje user rest controllera sa crud operacijama

--- user-service/src/main/java/rs/edu/raf/userservice/controllers/UserController.java
+++ user-service/src/main/java/rs/edu/raf/userservice/controllers/UserController.java
@@ -1,24 +1,77 @@
 package rs.edu.raf.userservice.controllers;
 
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import rs.edu.raf.userservice.domains.model.User;
+import rs.edu.raf.userservice.services.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
-    public UserController() {
-    }
+//    private final UserService userService;
 
-    @GetMapping
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public void getAllUsers() {
+
     }
+//    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User getUserById(@PathVariable Long id) {
+//
+////        return userService.findById(id).orElse(null);
+//    }
+//
+//    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User createUser(@RequestBody User user){
+//
+////        return userService.save(user);
+//    }
+//
+//    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User updateUser(@RequestBody User updatedUser){
+//
+////        return userService.save(updatedUser);
+//    }
+//
+//    @DeleteMapping(value = "/{id}")
+//    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+////        if(userService.findById(id) != null) {
+////            userService.deleteById(id);
+////            return ResponseEntity.ok().build();
+////        }
+////        return ResponseEntity.notFound().build();
+//    }
+    //    @GetMapping(value = "/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User getUserByEmail(@PathVariable String email) {
+//
+////        return userService.findByEmail(email).orElse(null);
+//    }
+//    @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+////    public User getUserByUsername(@PathVariable String username) {
+////
+//////        return userService.findByUsername(email).orElse(null);
+////    }
+    //    @GetMapping(value = "/{mobileNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User getUserByMobileNumber(@PathVariable String mobileNumber) {
+//
+////        return userService.findByMobileNumber(mobileNumber).orElse(null);
+//    }
+    //    @GetMapping(value = "/{jmbg}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User getUserByJMBG(@PathVariable String jmbg) {
+////        return userService.findByJmbg(jmbg).orElse(null);
+//
+//    }
+
+
 
 }
