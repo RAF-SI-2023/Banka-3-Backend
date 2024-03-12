2f77c57b1244bb0bf5b9a5e3f60a50ec65d0b2b5
Strahinja Ljubicic <sljubicic7120rn@raf.rs>
2024-03-12 18:15:49 +0100

    BCK-15 Popravka swagger dependency verzije

--- user-service/src/main/java/rs/edu/raf/userservice/configuration/SpringSecurityConfig.java
+++ user-service/src/main/java/rs/edu/raf/userservice/configuration/SpringSecurityConfig.java
@@ -1,27 +1,20 @@
 package rs.edu.raf.userservice.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import rs.edu.raf.userservice.filters.JwtFilter;
 import rs.edu.raf.userservice.services.UserService;
 
 @EnableWebSecurity
 @EnableAsync
-
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final JwtFilter jwtFilter;
 
@@ -46,28 +39,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                //TODO
-                //Role:ADMIN_USER,
-                //     READ_USERS,
-                //     CREATE_USERS,
-                //     UPDATE_USERS,
-                //     DELETE_USERS
-//                .antMatchers("/api/users/login").permitAll()
-//                .antMatchers("/api/users/add/**").hasAuthority("can_create_users")
-//                .antMatchers("/api/users/update/**").hasAuthority("can_update_users")
-
-
-
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().headers()
+                .frameOptions().disable();
 
         //Prestace da se crveni kada se UserService sredi. Cekamo kolege da zavrse to
         httpSecurity.addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
+
     @Override
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception{
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManager();
     }
 }
