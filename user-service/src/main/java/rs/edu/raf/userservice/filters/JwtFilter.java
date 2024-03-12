664bb17fb8a4c02fa1a10ad19dc0db77d7a8576a
strahinja-ljubicicRN71-20 <92782730+strahinja-ljubicicRN71-20@users.noreply.github.com>
2024-03-12 01:14:09 +0100

    Update JwtFilter.java

--- user-service/src/main/java/rs/edu/raf/userservice/filters/JwtFilter.java
+++ user-service/src/main/java/rs/edu/raf/userservice/filters/JwtFilter.java
@@ -2,11 +2,6 @@ package rs.edu.raf.userservice.filters;
 
 import io.jsonwebtoken.Jwt;
 
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
