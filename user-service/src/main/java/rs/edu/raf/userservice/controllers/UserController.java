78c02bd5a8143f660892a4a7021050333157e825
Ninoslav Zlatanovic <nzlatanovic7020rn@raf.rs>
2024-03-12 13:25:20 +0100

    BCK-4 Implementacija login i register endpointa

--- user-service/src/main/java/rs/edu/raf/userservice/controllers/UserController.java
+++ user-service/src/main/java/rs/edu/raf/userservice/controllers/UserController.java
@@ -1,6 +1,10 @@
 package rs.edu.raf.userservice.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +12,11 @@ import rs.edu.raf.userservice.domains.model.User;
 import rs.edu.raf.userservice.services.UserService;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import rs.edu.raf.userservice.domains.dto.LoginRequest;
+import rs.edu.raf.userservice.domains.dto.RegistrerUserDto;
+import rs.edu.raf.userservice.domains.model.User;
+import rs.edu.raf.userservice.services.UserService;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -23,7 +32,7 @@ public class UserController {
     public void getAllUsers() {
 
     }
-    
+
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserById(@PathVariable Long id) {
 
@@ -52,29 +61,59 @@ public class UserController {
        // }
        return ResponseEntity.notFound().build();
     }
-    
+
     @GetMapping(value = "/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserByEmail(@PathVariable String email) {
 
 ////        return userService.findByEmail(email).orElse(null);
     }
-    
+
     @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserByUsername(@PathVariable String username) {
 
 //////        return userService.findByUsername(email).orElse(null);
    }
-    
+
     @GetMapping(value = "/{mobileNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserByMobileNumber(@PathVariable String mobileNumber) {
 
 ////        return userService.findByMobileNumber(mobileNumber).orElse(null);
-    }    
-    
+    }
+
     @GetMapping(value = "/{jmbg}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserByJMBG(@PathVariable String jmbg) {
 ////        return userService.findByJmbg(jmbg).orElse(null);
 //
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(LoginRequest loginRequest) {
+
+        User user = userService.findByEmail(loginRequest.getEmail());
+
+        if(user != null) {
+            if(user.getPassword().equals(loginRequest.getPassword())) {
+                // ovde sada dodje jwt filter i generise token koji se vraca na front
+            }
+        }
+
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(RegistrerUserDto registrerUserDto) {
+
+        User user = User.builder()
+                .firstName(registrerUserDto.getFirstName())
+                .lastName(registrerUserDto.getLastName())
+                .jmbg(registrerUserDto.getJmbg())
+                .dateOfBirth(registrerUserDto.getDateOfBirth())
+                .build();
+
+        userService.addUser(user);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- user-service/src/main/java/rs/edu/raf/userservice/services/UserService.java
+++ user-service/src/main/java/rs/edu/raf/userservice/services/UserService.java
@@ -1,4 +1,21 @@
 package rs.edu.raf.userservice.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.edu.raf.userservice.domains.model.User;
+import rs.edu.raf.userservice.repositories.UserRepository;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
 }
