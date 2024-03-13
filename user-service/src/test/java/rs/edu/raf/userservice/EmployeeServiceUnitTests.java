0cd9939b71f51e2966d294c5264582cf77052096
npantos17 <72134672+npantos17@users.noreply.github.com>
2024-03-13 03:41:17 +0100

    Delete promenjen da menja zaposlenog na ne aktivnog.

--- user-service/src/test/java/rs/edu/raf/userservice/EmployeeServiceUnitTests.java
+++ user-service/src/test/java/rs/edu/raf/userservice/EmployeeServiceUnitTests.java
@@ -1,11 +1,18 @@
 package rs.edu.raf.userservice;
 
+import io.cucumber.java.an.E;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import rs.edu.raf.userservice.domains.dto.EmployeeDto;
+import rs.edu.raf.userservice.domains.model.Employee;
 import rs.edu.raf.userservice.repositories.EmployeeRepository;
 import rs.edu.raf.userservice.services.EmployeeService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceUnitTests {
@@ -15,6 +22,21 @@ public class EmployeeServiceUnitTests {
 
     @InjectMocks
     private EmployeeService employeeService;
+
+
+    @Test
+    void findByEmail(){
+        Employee employee = new Employee();
+        employee.setEmail("email@gmail.com");
+        given(employeeRepository.save(employee)).willReturn(employee);
+
+        try{
+            EmployeeDto employeeDto = employeeService.findByEmail("email@gmail.com");
+            assertNotNull(employeeDto);
+        }catch (Exception e){
+            fail();
+        }
+    }
 
 
 
