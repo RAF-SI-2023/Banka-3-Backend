4b921e5bb0ae6dcec0c61f12514db9c2e0495c58
ndavidov120rn <92782008+ndavidov120rn@users.noreply.github.com>
2024-03-13 14:25:58 +0100

    BCK-13 kreiranje employee controller (#30)

--- user-service/src/main/java/rs/edu/raf/userservice/controllers/EmoloyeeController.java
+++
@@ -1,4 +0,0 @@
-package rs.edu.raf.userservice.controllers;
-
-public class EmoloyeeController {
-}
---
+++ user-service/src/main/java/rs/edu/raf/userservice/controllers/EmployeeController.java
@@ -0,0 +1,80 @@
+package rs.edu.raf.userservice.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.edu.raf.userservice.services.EmoloyeeService;
+
+@RestController
+@RequestMapping("/api/v1/employee")
+public class EmployeeController {
+
+    /*
+    @Autowired
+    private EmoloyeeService emoloyeeService;
+
+    @Autowired
+    public EmployeeController(EmoloyeeService emoloyeeService){this.emoloyeeService=emoloyeeService;}
+
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto createEmployee(@RequestBody CreateEmployeeDto createEmployeeDto) {
+        return emoloyeeService.createEmployee(createEmployeeDto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findAllEmployees() {
+        return emoloyeeService.findAllEmployees();
+    }
+
+    @PutMapping(value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto updateEmployee(@RequestBody UpdateEmployeeDto updatedEmployee, @PathVariable Long id) {
+        return emoloyeeService.updateEmployee(updatedEmployee, id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+        if(emoloyeeService.findEmployeeById(id) != null) {
+            emoloyeeService.deactivateEmployee(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findEmployeeById(@PathVariable Long id) {
+        return emoloyeeService.findEmployeeById(id).orElse(null);
+    }
+
+    @GetMapping(value = "/findByEmail/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findEmployeeByEmail(@PathVariable String email) {
+        return emoloyeeService.findEmployeeByEmail(email);
+    }
+
+    @GetMapping(value = "/findByUsername/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findEmployeeByUsername(@PathVariable String username) {
+        return emoloyeeService.findEmployeeByUsername(username);
+    }
+
+    @GetMapping(value = "/findByMobileNumber/{mobileNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findEmployeeByMobileNumber(@PathVariable String mobileNumber) {
+        return emoloyeeService.findEmployeeByMobileNumber(mobileNumber);
+    }
+
+    @GetMapping(value = "/findByJmbg/{jmbg}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findEmployeeByJMBG(@PathVariable String jmbg) {
+        return emoloyeeService.findEmployeeByJmbg(jmbg);
+    }
+
+    @GetMapping(value = "/findByPosition/{position}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto findEmployeeByPosition(@PathVariable String position) {
+        return emoloyeeService.findEmployeeByPosition(position);
+    }
+
+     */
+}
