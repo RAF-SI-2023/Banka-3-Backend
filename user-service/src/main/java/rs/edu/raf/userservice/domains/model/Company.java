bd94a214923611bc61cf6d46e8eaa0f0cf4c01fc
mpopovic9720rn <92787732+mpopovic9720rn@users.noreply.github.com>
2024-03-17 22:08:39 +0100

    [BCK-38] Prosirivanje User Service-a novim entitetima (#69)

---
+++ user-service/src/main/java/rs/edu/raf/userservice/domains/model/Company.java
@@ -0,0 +1,48 @@
+package rs.edu.raf.userservice.domains.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@ToString
+public class Company implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long companyId;
+
+    @NotNull(message = "This field cannot be NULL")
+    private String title;
+
+    @NotNull(message = "This field cannot be NULL")
+    @Size(max = 20, message = "The input is too long")
+    private String number;
+
+    @NotNull(message = "This field cannot be NULL")
+    @Column(unique = true)
+    private int pib;
+
+    @NotNull(message = "This field cannot be NULL")
+    @Column(unique = true)
+    private int maticniBroj;
+
+    @NotNull(message = "This field cannot be NULL")
+    private int sifraDelatnosti;
+
+    @NotNull(message = "This field cannot be NULL")
+    @Email
+    private String email;
+
+
+
+}
