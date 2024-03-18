da0f16fc6b482d94c76c8974ca30a0978a8f0ff1
Matija Marjanovic <mmarjanovic11121rn@raf.rs>
2024-03-18 20:48:46 +0100

    Prilagodjavanje odradjenog postojecem projektu. Mala promena u arhitekturi.

--- email-service/src/main/java/com/example/emailservice/controller/CodeSenderController.java
+++ email-service/src/main/java/com/example/emailservice/controller/CodeSenderController.java
@@ -2,14 +2,9 @@ package com.example.emailservice.controller;
 
 
 import com.example.emailservice.dto.CodeSenderDto;
-import com.example.emailservice.dto.CodeSenderEntityDto;
-import com.example.emailservice.model.CodeSender;
-import com.example.emailservice.service.CodeSenderService;
 import com.example.emailservice.service.impl.CodeSenderServiceImpl;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,15 +16,10 @@ import org.springframework.web.bind.annotation.*;
 public class CodeSenderController {
 
     @Autowired
-    private CodeSenderService codeSenderService;
+    private CodeSenderServiceImpl codeSenderService;
     @PostMapping(value = "/activateUser", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> activateUser(@RequestBody CodeSenderDto codeSenderDto) {
         return codeSenderService.activateUser(codeSenderDto);
-    }
-    
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/findByCode/{code}")
-    public CodeSenderEntityDto findCodeSenderByCode(@PathVariable("code") Integer code){
-        return codeSenderService.findCodeSenderByCode(code);
     }
 }
