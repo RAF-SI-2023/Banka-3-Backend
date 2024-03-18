4c993416f12ed18713d050ced8a3f0fec7e84155
Matija Marjanovic <mmarjanovic11121rn@raf.rs>
2024-03-18 16:22:11 +0100

    Kreirane metode za aktivaciju naloga korisnika. U servisu dodata metoda activateUser koja u trazenim uslovima aktivira nalog korisniku.s

--- email-service/src/main/java/com/example/emailservice/repositories/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.emailservice.repositories;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository {
-}
---
+++ email-service/src/main/java/com/example/emailservice/services/CodeSenderService.java
@@ -0,0 +1,2 @@
+package com.example.emailservice.services;public class CodeSenderService {
+}
