fe654e77b9a63c8d190c022b757c50f7da6bd539
mljubinkovic <mljubinkovic9120rn@raf.rs>
2024-03-18 21:17:37 +0100

    BCK-34-2 Aktivacija naloga zaposlenog, dodati komentari

--- email-service/src/main/java/com/example/emailservice/service/impl/EmployeeServiceImpl.java
+++ email-service/src/main/java/com/example/emailservice/service/impl/EmployeeServiceImpl.java
@@ -7,13 +7,13 @@ import com.example.emailservice.repository.EmployeeActivationRepository;
 import com.example.emailservice.service.EmailService;
 import com.example.emailservice.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import static java.lang.Thread.sleep;
@@ -24,6 +24,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeActivationRepository employeeActivationRepository;
     private final UserServiceClient userServiceClient;
     private final EmailService emailService;
+    /***
+     *Pravi se random identifier, objekat employeeActivation i cuva se u bazu
+     *Posalje se mejl i onda se startuje timer da se za 5 minuta prebaci u false
+     *
+     */
     @Override
     public void employeeCreated(String email) {
         String identifier = UUID.randomUUID().toString();
@@ -36,7 +41,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeActivationRepository.save(employeeActivation);
         emailService.sendSimpleMessage(email, getSubject(), getText(identifier));
         new Thread(()->{
-            long activationAvailableTime = 5*60*60;
+            long activationAvailableTime = 5*60*1000;
             try {
                 sleep(activationAvailableTime);
                 employeeActivation.setActivationPossible(false);
@@ -46,14 +51,19 @@ public class EmployeeServiceImpl implements EmployeeService {
             }
         }).start();
     }
-
+    /***
+     *Prvo se trazi u bazi da li moze nalog da se aktivira(ako ne moze baca bad request exception)
+     *Ako postoji u bazi email i sifra se salju na userService.
+     *Proverava se da li je response 200 i ako jeste stize poruka da je sifra uspesno promenjena
+     */
     @Override
     public String changePassword(String identifier, String password) {
-        EmployeeActivation employeeActivation =
-                employeeActivationRepository.findEmployeeActivationByIdentifierAndActivationPossibleIsTrue(identifier)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Activation is not possible"));
+        Optional<EmployeeActivation> employeeActivationOptional =
+                employeeActivationRepository.findEmployeeActivationByIdentifierAndActivationPossibleIsTrue(identifier);
+        EmployeeActivation employeeActivation = employeeActivationOptional.get();
         SetPasswordDTO passwordDTO = new SetPasswordDTO(password, employeeActivation.getEmail());
         ResponseEntity<String> response = userServiceClient.setPassword(passwordDTO);
+        System.out.println(response.toString());
         if(!response.getStatusCode().is2xxSuccessful()){
             throw new ResponseStatusException(response.getStatusCode(), response.getBody());
         }
@@ -64,6 +74,6 @@ public class EmployeeServiceImpl implements EmployeeService {
         return "Employee account activation";
     }
     protected String getText(String identifier){
-        return "http://localhost:8081/employee/changePassword/" + identifier;
+        return "http://localhost:8081/employee/setPassword/" + identifier;
     }
 }
