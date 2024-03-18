c3f16d52f6fcec6bea76ecf581b8cdd389bf4f20
Matija Marjanovic <mmarjanovic11121rn@raf.rs>
2024-03-18 15:09:40 +0100

    Kreirane klase service, repository i controller za email service. Kreirani entiteti u tabeli.

---
+++ email-service/src/main/java/com/example/emailservice/services/EmailService.java
@@ -0,0 +1,10 @@
+package com.example.emailservice.services;
+
+import com.example.emailservice.repositories.EmailRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    //private final EmailRepository emailRepository;
+}
