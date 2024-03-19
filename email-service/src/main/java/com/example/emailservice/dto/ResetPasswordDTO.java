662cd2910bbe57c80433d3f97689ba89257f8b4b
Jelena Drljan <jdrljan4620rn@raf.rs>
2024-03-19 22:36:32 +0100

    BCK-45, treba da se iskomentarise i testira, jos nije gotovo

---
+++ email-service/src/main/java/com/example/emailservice/dto/ResetPasswordDTO.java
@@ -0,0 +1,16 @@
+package com.example.emailservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetPasswordDTO implements Serializable {
+    private String password;
+    private String email;
+
+}
