61aa5c9546db9865f892f6d214bb42a935fe549f
npantos17 <72134672+npantos17@users.noreply.github.com>
2024-03-20 22:26:09 +0100

    Add user email controller and reset password route

---
+++ email-service/src/main/java/com/example/emailservice/controller/UserEmailController.java
@@ -0,0 +1,34 @@
+package com.example.emailservice.controller;
+
+import com.example.emailservice.service.EmailService;
+import com.example.emailservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@RequiredArgsConstructor
+public class UserEmailController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("resetPassword")
+    public String resetPassword() {
+//        employeeService.employeeCreated(email);
+        return  "radi";
+    }
+
+
+
+
+}
