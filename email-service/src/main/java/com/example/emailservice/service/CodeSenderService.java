0a85f76a88cb8030a390a3d6aa9421d9eb52ba95
Matija Marjanovic <mmarjanovic11121rn@raf.rs>
2024-03-20 21:55:12 +0100

    Istestirana funkcionalnost, radi.

--- email-service/src/main/java/com/example/emailservice/service/CodeSenderService.java
+++ email-service/src/main/java/com/example/emailservice/service/CodeSenderService.java
@@ -1,9 +1,12 @@
 package com.example.emailservice.service;
 import com.example.emailservice.dto.CodeSenderDto;
 
+import com.example.emailservice.dto.CodeSenderEntityDto;
 import org.springframework.http.ResponseEntity;
 
 public interface CodeSenderService {
 
     ResponseEntity<String> activateUser(CodeSenderDto codeSenderDto);
+
+    CodeSenderEntityDto findCodeSenderByCode(Integer code);
 }
