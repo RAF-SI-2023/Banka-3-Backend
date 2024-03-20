bb2e5dca1e4f45ca06d70b37155f08327b26c38b
mljubinkovic <72108092+mljubinkovic@users.noreply.github.com>
2024-03-20 19:07:08 +0100

    BCK-34-2 Aktivacija naloga zaposlenog, dodati komentari (#79)

--- email-service/src/main/java/com/example/emailservice/controller/EmailController.java
+++ email-service/src/main/java/com/example/emailservice/controller/EmailController.java
@@ -5,20 +5,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @RequestMapping(value = "api/v1/employee")
 @RequiredArgsConstructor
 public class EmailController {
     private final EmployeeService employeeService;
+
+    /***
+     *Kada se kreira employee, user service salje request na employeeCreated da bi se napravio
+     *identifier koji vazi 5 minuta i
+     *
+     */
     @GetMapping("employeeCreated")
     public ResponseEntity<Void> employeeCreated(@RequestParam(name = "email") String email) {
         employeeService.employeeCreated(email);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
-    @PostMapping("changePassword/{identifier}")
+
+    @PostMapping("/setPassword/{identifier}")
     public ResponseEntity<String> changePassword(@PathVariable(name = "identifier") String identifier,
                                                  @RequestBody String password){
-        return ResponseEntity.ok(employeeService.changePassword(identifier, password));
+        String response = employeeService.changePassword(identifier, password);
+        return ResponseEntity.ok(response);
     }
 }
