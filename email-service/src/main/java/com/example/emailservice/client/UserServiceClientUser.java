80da8fabc4f5d03cdc2eb275e6caaf1a9f40ac20
Matija Marjanovic <mmarjanovic11121rn@raf.rs>
2024-03-20 17:22:17 +0100

    Preimenovan feign client

--- email-service/src/main/java/com/example/emailservice/client/UserServiceClientUser.java
+++ email-service/src/main/java/com/example/emailservice/client/UserServiceClientUser.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "userServiceClient", url = "${userServiceLocation}/user")
+@FeignClient(name = "userServiceClientUser", url = "${userServiceLocation}/user")
 public interface UserServiceClientUser {
 
     @PostMapping(value = "/setPassword",
