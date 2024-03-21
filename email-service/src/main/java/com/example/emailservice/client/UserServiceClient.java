c8477df1af8570b74b0b01634faec8bed0ad5fed
npantos17 <72134672+npantos17@users.noreply.github.com>
2024-03-21 01:07:50 +0100

    Update UserServiceClient.java

--- email-service/src/main/java/com/example/emailservice/client/UserServiceClient.java
+++ email-service/src/main/java/com/example/emailservice/client/UserServiceClient.java
@@ -1,16 +1,22 @@
 package com.example.emailservice.client;
 
+import com.example.emailservice.dto.ResetUserPasswordDTO;
 import com.example.emailservice.dto.SetPasswordDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "userServiceClient", url = "${userServiceLocation}/employee")
+@FeignClient(name = "userServiceClient", url = "${userServiceLocation}")
 public interface UserServiceClient {
-    @PostMapping(value = "/setPassword",
+    @PostMapping(value = "/employee/setPassword",
             produces = "application/json",
             consumes = "application/json")
     ResponseEntity<String> setPassword(@RequestBody SetPasswordDTO passwordDTO);
+
+    @PostMapping(value = "/user/resetPassword",
+            produces = "application/json",
+            consumes = "application/json")
+    ResponseEntity<String> resetUserPassword(@RequestBody ResetUserPasswordDTO passwordDTO);
 
 }
