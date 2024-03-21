2c895820ae6ab526535035122078aec9553afd7a
Strahinja Ljubicic <sljubicic7120rn@raf.rs>
2024-03-21 10:50:37 +0100

    Revert "BCK-36-2 Aktivacija naloga korisnika (nedovrsena) (#72)"
    This reverts commit 1c9f5b33

--- email-service/src/main/java/com/example/emailservice/service/CodeSenderService.java
+++
@@ -1,9 +0,0 @@
-package com.example.emailservice.service;
-import com.example.emailservice.dto.CodeSenderDto;
-
-import org.springframework.http.ResponseEntity;
-
-public interface CodeSenderService {
-
-    ResponseEntity<String> activateUser(CodeSenderDto codeSenderDto);
-}
