3d820c2e84f6847a9af6f96c4c9b2ca81a49a2ba
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2024-03-27 22:24:59 +0100

    Update ContactRepository.java

--- user-service/src/main/java/rs/edu/raf/userservice/repositories/ContactRepository.java
+++ user-service/src/main/java/rs/edu/raf/userservice/repositories/ContactRepository.java
@@ -1,12 +1,13 @@
 package rs.edu.raf.userservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rs.edu.raf.userservice.domains.model.Contact;
 import rs.edu.raf.userservice.domains.model.Currency;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
     Optional<List<Contact>> findByUser_UserId(Long id);
