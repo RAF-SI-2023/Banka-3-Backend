4bdd6a03b83a96e59e97940c3ee2efed62095d3b
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2024-03-27 22:24:55 +0100

    Update ContactMapper.java

--- user-service/src/main/java/rs/edu/raf/userservice/domains/mappers/ContactMapper.java
+++ user-service/src/main/java/rs/edu/raf/userservice/domains/mappers/ContactMapper.java
@@ -1,10 +1,14 @@
 package rs.edu.raf.userservice.domains.mappers;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import rs.edu.raf.userservice.domains.dto.contact.ContactCreateDto;
 import rs.edu.raf.userservice.domains.dto.contact.ContactDto;
 import rs.edu.raf.userservice.domains.model.Contact;
-
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ContactMapper {
 
     ContactMapper INSTANCE = Mappers.getMapper(ContactMapper.class);
