6a99f4bcd5c88ae00ecce72dd3eca888baa4019e
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2024-03-27 22:24:47 +0100

    Update ContactUpdateDto.java

--- user-service/src/main/java/rs/edu/raf/userservice/domains/dto/contact/ContactUpdateDto.java
+++ user-service/src/main/java/rs/edu/raf/userservice/domains/dto/contact/ContactUpdateDto.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContactUpdateDto {
-
     private String myName;
     private String name;
     private String accountNumber;
