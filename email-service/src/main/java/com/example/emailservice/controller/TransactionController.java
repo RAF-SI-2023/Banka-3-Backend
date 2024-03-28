f75557721bb78a718703584db8c856727589da19
Strahinja Ljubicic <sljubicic7120rn@raf.rs>
2024-03-28 14:33:18 +0100

    Cross origin

--- email-service/src/main/java/com/example/emailservice/controller/TransactionController.java
+++ email-service/src/main/java/com/example/emailservice/controller/TransactionController.java
@@ -7,13 +7,11 @@ import com.example.emailservice.service.impl.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/transaction")
+@CrossOrigin
 @RequiredArgsConstructor
 public class TransactionController {
 
--- email-service/src/main/java/com/example/emailservice/controller/UserEmailController.java
+++ email-service/src/main/java/com/example/emailservice/controller/UserEmailController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/user")
+@CrossOrigin
 @RequiredArgsConstructor
 public class UserEmailController {
 
