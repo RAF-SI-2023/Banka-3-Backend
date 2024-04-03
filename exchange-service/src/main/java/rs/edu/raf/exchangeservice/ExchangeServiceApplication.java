0d5fc2fb9ee0cab8140791e31271c2b1465d0998
Strahinja Ljubicic <strahinja.ljubicic@united.cloud>
2024-04-03 12:48:01 +0200

    Exchange service setup

---
+++ exchange-service/src/main/java/rs/edu/raf/exchangeservice/ExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.exchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeServiceApplication.class, args);
+	}
+
+}
