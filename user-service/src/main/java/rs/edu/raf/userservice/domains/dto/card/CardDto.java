d5cd66fd1f286bc2cf24ff9426cba8faae1f5f11
dpopovic99 <dpopovic10720rn@raf.rs>
2024-04-11 11:51:36 +0200

    premesteno iz bank u user service i dodate withdraw/deposit/cardlogin u controller

---
+++ user-service/src/main/java/rs/edu/raf/userservice/domains/dto/card/CardDto.java
@@ -0,0 +1,16 @@
+package rs.edu.raf.userservice.domains.dto.card;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
+public class CardDto {
+    private String cardNumber;
+    private Long expireDate;
+    private String cvc;
+}
