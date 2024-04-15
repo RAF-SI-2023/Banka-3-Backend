c82a31e02cab06f47fc0a11c7c4675cea160a64b
Matija Marjanovic <mmarjanovic11121rn@raf.rs>
2024-04-15 17:23:19 +0200

    Integracioni testovi za accountTypeController.feature gotovi, postavljena baza za cardController.feature

---
+++ user-service/src/test/java/rs/edu/raf/userservice/integration/cardController/CardControllerTestsConfig.java
@@ -0,0 +1,9 @@
+package rs.edu.raf.userservice.integration.cardController;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CardControllerTestsConfig {
+}
---
+++ user-service/src/test/java/rs/edu/raf/userservice/integration/cardController/CardControllerTestsState.java
@@ -0,0 +1,14 @@
+package rs.edu.raf.userservice.integration.cardController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardControllerTestsState {
+    String jwtToken;
+}
