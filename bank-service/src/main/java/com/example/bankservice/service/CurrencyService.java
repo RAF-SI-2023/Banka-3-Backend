1d58a1a98732eff829abda57d5f64f5e290fe8e9
Strahinja Ljubicic <strahinja.ljubicic@united.cloud>
2024-04-20 14:32:48 +0200

    Add currency controller

--- bank-service/src/main/java/com/example/bankservice/service/CurrencyService.java
+++ bank-service/src/main/java/com/example/bankservice/service/CurrencyService.java
@@ -1,4 +1,24 @@
 package com.example.bankservice.service;
 
+import com.example.bankservice.domain.model.Currency;
+import com.example.bankservice.repository.CurrencyRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class CurrencyService {
+
+    private CurrencyRepository currencyRepository;
+
+    public Currency findByMark(String currencyMark) {
+        return currencyRepository.findByMark(currencyMark)
+                .orElseThrow(() -> new RuntimeException("Currency not found"));
+    }
+
+    public List<Currency> findAll() {
+        return currencyRepository.findAll();
+    }
 }
