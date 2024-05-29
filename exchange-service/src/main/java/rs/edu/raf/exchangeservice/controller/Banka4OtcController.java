62b6010abd4dbf48b9328920dec96152fc7e9e3e
npantos17 <72134672+npantos17@users.noreply.github.com>
2024-05-29 23:08:04 +0200

    Neke stvari uradjene polovicno

--- exchange-service/src/main/java/rs/edu/raf/exchangeservice/controller/Banka4OtcController.java
+++ exchange-service/src/main/java/rs/edu/raf/exchangeservice/controller/Banka4OtcController.java
@@ -1,4 +1,45 @@
 package rs.edu.raf.exchangeservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.edu.raf.exchangeservice.domain.dto.offer.OfferDto;
+import rs.edu.raf.exchangeservice.service.Banka4OtcService;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/otcTrade")
 public class Banka4OtcController {
+
+    private final Banka4OtcService banka4OtcService;
+
+    @GetMapping("/getOffers")
+    @Operation(description = "dohvata stockove nase banke")
+    public ResponseEntity<?> getOffers(){
+        return ResponseEntity.ok(banka4OtcService.findAllOffers());
+    }
+    @GetMapping("/getStocks")
+    @Operation(description = "dohvata stockove nase banke")
+    public ResponseEntity<?> getMyStocks(){
+        return ResponseEntity.ok(banka4OtcService.findAllStocks());
+    }
+
+    @GetMapping("/getBank4Stocks")
+    @Operation(description = "dohvata stockove od banke 4")
+    public ResponseEntity<?> getBank4Stocks(){
+        banka4OtcService.getBank4Stocks();
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/sendOffer")
+    @Operation(description = "primamo ponudu od banke 4")
+    public ResponseEntity<?> receiveOffer(@RequestBody OfferDto dto){
+        banka4OtcService.receiveOffer(dto);
+        return ResponseEntity.ok().build();
+
+    }
+
 }
---
+++ exchange-service/src/main/java/rs/edu/raf/exchangeservice/repository/offerRepository/OfferRepository.java
@@ -0,0 +1,14 @@
+package rs.edu.raf.exchangeservice.repository.offerRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import rs.edu.raf.exchangeservice.domain.model.offer.Offer;
+
+@Repository
+@Transactional(isolation = Isolation.SERIALIZABLE)
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+
+}
