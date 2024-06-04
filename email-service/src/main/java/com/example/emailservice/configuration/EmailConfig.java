50c05b49fdd9258702d415ff819ecfe42ca840fb
Strahinja Ljubicic <strahinja.ljubicic@united.cloud>
2024-06-04 23:01:45 +0200

    Set-pop

--- email-service/src/main/java/com/example/emailservice/configuration/EmailConfig.java
+++ email-service/src/main/java/com/example/emailservice/configuration/EmailConfig.java
@@ -12,7 +12,7 @@ public class EmailConfig {
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+        mailSender.setHost("pop.gmail.com");
         mailSender.setPort(587);
 
         mailSender.setUsername("banak.tri@gmail.com");
