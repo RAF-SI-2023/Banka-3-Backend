a22dfce5ed2ac689e6e9ce67b38f178a3a96b6d0
Janko <162360001+zews-coder@users.noreply.github.com>
2024-06-22 14:40:22 +0200

    dodata ruta za birsanje offera (#238)

--- exchange-service/src/main/java/rs/edu/raf/exchangeservice/service/BankOtcService.java
+++ exchange-service/src/main/java/rs/edu/raf/exchangeservice/service/BankOtcService.java
@@ -391,13 +391,26 @@ public class BankOtcService {
         return false;
     }
 
-    //DELETE: /deleteOffer/id
-    //kada treba neka ponuda da se obrise iz baze
-    public boolean deleteOffer(Long id){
+    //DELETE: /deleteMyOffer/id
+    //kada treba neka nasu ponuda da obrisemo iz baze
+    public boolean deleteMyOffer(Long id){
         Optional<MyOffer> myOfferOptional = myOfferRepository.findById(id);
         if(myOfferOptional.isPresent()) {
             MyOffer myOffer = myOfferOptional.get();
             myOfferRepository.delete(myOffer);
+            return true;
+        }
+
+        return false;
+    }
+
+    //DELETE: /deleteOffer/id
+    //kada treba neka tudju ponuda da obrisemo iz baze
+    public boolean deleteOffer(Long id){
+        Optional<Offer> offerOptional = offerRepository.findById(id);
+        if(offerOptional.isPresent()) {
+            Offer offer = offerOptional.get();
+            offerRepository.delete(offer);
             return true;
         }
 
